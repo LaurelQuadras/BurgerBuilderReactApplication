67475cafc79f858dca160fd953b686f511cf71f1
laurel quadras <laurelquadras@gmail.com>
2019-09-01 23:20:55 +0530

    Added Navigations, BurgerLogo, Toolbar, Backdrop

--- burger-builer/src/components/Layout/Layout.js
+++ burger-builer/src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Auxillary from '../../hoc/Auxillary';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout = ( props ) => (
     <Auxillary>
         <div>
-            Toolbar, Sidebar, Backdrop
+            <Toolbar />
         </div>
         <main className={classes.Content}>
             {props.children}
