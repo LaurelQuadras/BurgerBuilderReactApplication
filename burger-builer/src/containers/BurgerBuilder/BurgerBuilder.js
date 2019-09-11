d18fe5f29d5abd1762de7a6720fd3952288bd57e
laurel quadras <laurelquadras@gmail.com>
2019-09-11 20:02:02 +0530

    Added Routing to the project using axios

--- burger-builer/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builer/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,28 +87,19 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
 //        alert('You continue!');
-        this.setState({loading: true})
+        
+        const queryParams = [];
 
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Laurel Joel Quadras',
-                address: {
-                    street: 'Ejipura',
-                    zipCode: '560034',
-                    country: 'Germany'
-                },
-                email: 'laurelquadras@gmail.com'
-            },
-            deliveryMethod: 'fastest'
-        };
-        axios.post('/orders.json', order)
-        .then(response => {
-            this.setState({loading: false, purchasing: false})
-        }).catch(error => {
-            this.setState({loading: false, purchasing: false})
-        })
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+        queryParams.push('price='+this.state.totalPrice);
+
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString 
+        });
     }
 
     render () {
